5045077846e19cbdb9ff7c52bac3a7b2e6f1aeaf
manap1031 <ikme.sabosan00@gmail.com>
2020-08-22 10:39:42 +0900

    first-commit

---
+++ app/controllers/donations_controller.rb
@@ -0,0 +1,19 @@
+class DonationsController < ApplicationController
+  def index
+  end
+
+  def new
+    @donation = User.new
+  end
+
+  def create
+    @donation = User.create(donation_params)
+  end
+
+  private
+
+  def donation_params
+    params.require(:user).permit(:name, :name_reading, :nickname)
+  end
+end
+
